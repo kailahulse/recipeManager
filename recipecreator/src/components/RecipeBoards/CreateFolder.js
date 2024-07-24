2e77efa0a9d3b93cbb3efac2594b30e4f4aa508a
kailahulse <102646005+kailahulse@users.noreply.github.com>
2024-07-23 18:58:44 -0700

    Able to add multiple forms

--- recipecreator/src/components/RecipeBoards/CreateFolder.js
+++ recipecreator/src/components/RecipeBoards/CreateFolder.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 
-function CreateFolder () {
-    const [open, setIsOpen] = React.useState(false);
+function CreateFolder() {
+    const [open, setIsOpen] = useState(false);
+    const [displayText, setDisplayText] = useState('');
+    const [formData, setFormData] = useState({});
+    const [allFormsData, setAllFormsData] = useState([]);
 
     const openForm = () => setIsOpen(true);
-    
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setDisplayText(formData.ingredients);
+        setAllFormsData(prevFormsData => [...prevFormsData, formData]);
+        setFormData({}); // Reset formData for a new form
+        setIsOpen(false);
+    };
+
     return (
         <div>
-            <button onClick={openForm}> Add new Folder</button>
-            {/* <Form open={open}/> */}
+            <button onClick={openForm}>Add new Folder</button>
             {open && (
-            <form>
-                <label>
-                    Enter in Steps:
-                    <input 
-                        type='text'
-
-                    />
-                </label>
-
-                </form>)}
-
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Enter in Steps:
+                        <textarea
+                            name="ingredients"
+                            cols="40"
+                            rows="5"
+                            className='boxStyle'
+                            onChange={handleChange}
+                        />
+                        <button type="submit">Done</button>
+                    </label>
+                </form>
+            )}
+            <div>
+                {allFormsData.map((form, index) => (
+                    <div key={index} className='centerText' style={{ whiteSpace: 'pre-wrap' }}>
+                        <h4>{`Form ${index + 1}`} Ingredients:</h4>
+                        <h4 key={index} className='centerText' style={{ whiteSpace: 'pre-wrap' }}>{form.ingredients}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
 
 export default CreateFolder;
